96a63a80a717b26112ae142bfab9aaf829330284
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-19 01:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,36 @@ function showResults(json) {
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+//use this function to create an issue based on the values input in index.html
+ const repo = 'tracdnguyen/js-ajax-fetch-lab';
+ const url = `https://api.github.com/repos/${repo}/issues`;
+ const postData = {
+   title: document.getElementById('title').value,
+   body: document.getElementById('body').value
+ };
 
+ fetch(url, {
+   method: 'POST',
+   body: JSON.stringify(postData),
+   headers: {
+     Authorization: `token ${getToken()}`
+   }
+ })
+  .then(res => res.json())
+  .then(json => getIssues());
 }
 
+
 function getIssues() {
+  const repo = 'tracdnguyen/js-ajax-fetch-lab';
+  const url = `https://api.github.com/repos/${repo}/issues`;
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+   .then(res => res.json())
+   .then(json => console.log(json));
   //once an issue is submitted, fetch all open issues to see the issues you are creating
 }
