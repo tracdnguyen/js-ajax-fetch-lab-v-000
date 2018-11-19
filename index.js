5d931462c61fa55e0912bc859cc24e4c9df850ed
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-19 00:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return '2ef53508d261c1a7d2359ee64be22abec293cd4e';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+    .then(json => showResults(json))
 }
-
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`
 }
 
 function createIssue() {
